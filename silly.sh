0c3d8469404c169c499fc83cd2e1500024480c74
calingoia2001 <70017658+calingoia2001@users.noreply.github.com>
2024-03-14 16:15:33 +0200

    Update silly.sh

--- silly.sh
+++ silly.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "This is a silly script V2" > sudo tee /usr/local/silly.txt > /dev/null
+echo "This is a silly script V2" > | sudo tee /usr/local/silly.txt > /dev/null
 sudo mkdir test{1..10}
