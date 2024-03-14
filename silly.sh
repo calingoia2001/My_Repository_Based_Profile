aa61712629df42609e9dfc74aebe530ef87e9838
calingoia2001 <70017658+calingoia2001@users.noreply.github.com>
2024-03-14 16:39:39 +0200

    Update silly.sh

--- silly.sh
+++ silly.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-# Define the file path
-file_path="/usr/local/test_file.txt"
-
-# Write "test" to the file with elevated privileges
-sudo echo "test" > "$file_path"
-
-# Provide feedback
-echo "Successfully wrote 'test' to $file_path"
+sudo su -
