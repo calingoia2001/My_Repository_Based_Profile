fba6bdfff0cbbd6fbd872babb771462c7f8e862d
calingoia2001 <70017658+calingoia2001@users.noreply.github.com>
2024-03-26 17:26:23 +0200

    Update profile.py

--- profile.py
+++ profile.py
@@ -22,32 +22,16 @@ request = pc.makeRequestRSpec()
 # Add a raw PC to the request.
 node1 = request.RawPC("node1_rawPC")
 node1.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD"
-iface0 = node1.addInterface('if1', pg.IPv4Address('192.168.1.1','255.255.255.0'))
-iface0.component_id = 'eth1'
 
 node2 = request.RawPC("node2_rawPC_withDocker")
 node2.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD"
-iface1 = node2.addInterface('if2', pg.IPv4Address('192.168.1.2','255.255.255.0'))
-iface1.component_id = 'eth2'
-
 
 node3 = request.XenVM("node3_xenVM")
 node3.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD"
-iface2 = node3.addInterface('if3', pg.IPv4Address('192.168.1.3','255.255.255.0'))
-iface2.component_id = 'eth3'
+
 
 node4 = request.XenVM("node4_xenVM_withDocker")
 node4.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD"
-iface3 = node4.addInterface('if4', pg.IPv4Address('192.168.1.4','255.255.255.0'))
-iface3.component_id = 'eth4'
 
-# Link lan
-link_lan = request.LAN('lan')
-link_lan.Site('undefined')
-link_lan.addInterface(iface0)
-link_lan.addInterface(iface1)
-link_lan.addInterface(iface2)
-link_lan.addInterface(iface3)
-
-# Print the RSpec to the enclosing page.
+# Prit the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
