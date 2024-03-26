b5fde005c37f26963048473b3a1417ae882b0d63
calingoia2001 <70017658+calingoia2001@users.noreply.github.com>
2024-03-26 17:15:13 +0200

    Update profile.py

--- profile.py
+++ profile.py
@@ -10,6 +10,8 @@ Wait for the profile instance to start, then click on the node in the topology a
 import geni.portal as portal
 # Import the ProtoGENI library.
 import geni.rspec.pg as pg
+# Import the Emulab specific extensions.
+import geni.rspec.emulab as emulab
 
 # Create a portal context.
 pc = portal.Context()
@@ -18,14 +20,34 @@ pc = portal.Context()
 request = pc.makeRequestRSpec()
  
 # Add a raw PC to the request.
-node = request.RawPC("my_node1_test")
+node1 = request.RawPC("node1_rawPC")
+node1.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD"
+iface0 = node1.addInterface('if1', pg.IPv4Address('192.168.1.1','255.255.255.0'))
+iface0.component_id = 'eth1'
 
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD"
+node2 = request.RawPC("node2_rawPC_withDocker")
+node2.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD"
+iface1 = node2.addInterface('if2', pg.IPv4Address('192.168.1.2','255.255.255.0'))
+iface1.component_id = 'eth2'
 
-# Install and execute a script that is contained in the repository.
-node.addService(pg.Execute(shell="bash", command="cd /local/repository"))
-node.addService(pg.Execute(shell="bash", command="chmod +x silly.sh"))
-node.addService(pg.Execute(shell="bash", command="./silly.sh"))
+
+node3 = request.XenVM("node3_xenVM")
+node3.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD"
+iface2 = node3.addInterface('if1', pg.IPv4Address('192.168.1.3','255.255.255.0'))
+iface2.component_id = 'eth3'
+
+node4 = request.XenVM("node4_xenVM_withDocker")
+node4.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD"
+iface3 = node43.addInterface('if1', pg.IPv4Address('192.168.1.4','255.255.255.0'))
+iface3.component_id = 'eth4'
+
+# Link lan
+link_lan = request.LAN('lan')
+link_lan.Site('undefined')
+link_lan.addInterface(iface0)
+link_lan.addInterface(iface1)
+link_lan.addInterface(iface2)
+link_lan.addInterface(iface4)
 
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
