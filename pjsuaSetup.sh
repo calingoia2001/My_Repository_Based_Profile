86b1c3153f23e0802ece2fb67c82a06b25af0150
calingoia2001 <70017658+calingoia2001@users.noreply.github.com>
2024-04-03 11:53:16 +0300

    Create pjsuaSetup.sh

---
+++ pjsuaSetup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt-get update
+apt install vim
+tar -xvzf pjproject-2.14.1.tar.gz
+cd pjproject-2.14.1/
+./configure CFLAGS="-fPIC"
+make dep
+make
+apt-get install swig -y
+apt-get install python3-dev -y
+cd pjsip-apps/src/swig/python
+make
+make install
